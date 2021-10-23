92a95e14b3136104eadeb72f677036917fe39f35
omerthecoder <86931960+omerthecoder@users.noreply.github.com>
2021-10-23 21:54:50 +0300

    Add files via upload

---
+++ SayiTahminiOyunu/SayiTahminiOyunu/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace SayiTahminiOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblAsagiYukari = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Şansını Dene";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblAsagiYukari
+            // 
+            this.lblAsagiYukari.AutoSize = true;
+            this.lblAsagiYukari.Location = new System.Drawing.Point(147, 48);
+            this.lblAsagiYukari.Name = "lblAsagiYukari";
+            this.lblAsagiYukari.Size = new System.Drawing.Size(0, 13);
+            this.lblAsagiYukari.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(129, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Oyuna Başla";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 83);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblAsagiYukari);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sayı Tahmini Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblAsagiYukari;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ SayiTahminiOyunu/SayiTahminiOyunu/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayiTahminiOyunu
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int tutulanSayi;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(textBox1.Text)==tutulanSayi)
+            {
+                lblAsagiYukari.Text = "Tebrikler sayıyı doğru tahmin ettiniz.";
+                btnStart.Enabled = true;
+                button1.Enabled = false;
+                textBox1.Enabled = false;
+            }
+            else if (Convert.ToInt32(textBox1.Text) < tutulanSayi)
+            {
+                lblAsagiYukari.Text = "Daha yüksek bir sayı giriniz.";
+            }
+            else if (Convert.ToInt32(textBox1.Text) > tutulanSayi)
+            {
+                lblAsagiYukari.Text="Daha düşük bir sayı giriniz.";
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tutulanSayi = rnd.Next(0, 100);
+            btnStart.Enabled = false;
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+        }
+    }
+}
