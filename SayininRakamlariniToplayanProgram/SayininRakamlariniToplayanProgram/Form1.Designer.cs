e0ab6de7ffe533d023eb7fe5c4c4bd406ff6a168
omerthecoder <86931960+omerthecoder@users.noreply.github.com>
2021-10-23 19:53:17 +0300

    Add files via upload

---
+++ SayininRakamlariniToplayanProgram/SayininRakamlariniToplayanProgram/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace SayininRakamlariniToplayanProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(12, 12);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(147, 20);
+            this.txtSayi.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Rakam toplamı hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(171, 110);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SayininRakamlariniToplayanProgram/SayininRakamlariniToplayanProgram/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayininRakamlariniToplayanProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sayi = txtSayi.Text;
+            int toplam=0;
+            try
+            {
+                for (int i = 0; i < sayi.Length; i++)
+                {
+                    toplam += Convert.ToInt32(sayi[i].ToString());
+                }
+                MessageBox.Show(toplam.ToString());
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
